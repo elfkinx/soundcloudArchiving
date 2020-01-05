5c00cbdaa545182b8fc49cd4228ad37b7067fcb7
elf (liam) <verticalwallmonkey+github@gmail.com>
2020-01-05 11:44:32 +0000

    Added logging; refactoring

---
+++ definitions.py
@@ -0,0 +1,4 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
+DATA_PATH = os.path.join(ROOT_DIR, 'data')  # requires `import os`
--- src/main/python/scdownload.py
+++ src/main/python/scdownload.py
@@ -2,26 +2,48 @@ import m3u8
 import requests
 import tempfile
 import functools
-from pydub import AudioSegment
 import re
+import logging
+import os
+from pydub import AudioSegment
+from definitions import DATA_PATH
+
+logging.basicConfig(level=logging.INFO,format="scdownload - INFO: %(message)s")
+logger = logging.getLogger("scdownload")
 
 client_id = "r5ELVSy3RkcjX7ilaL7n2v1Z8irA9SL8"
-url = "https://soundcloud.com/yung-bruh-3/gemstone-bullets-poppin-out-the-tech"
 
+# url = "https://soundcloud.com/yung-bruh-3/gemstone-bullets-poppin-out-the-tech"
+logger.info("Enter the Souncloud URL of the track to download: ")
+url = input()
+
+logger.info("Requesting track url: " + url)
 html = requests.get(url)
 track_id = re.search(r'soundcloud://sounds:(.+?)"', html.text).group(1)
+logger.info("Track ID: " + track_id)
 
 track_info_url = "https://api-v2.soundcloud.com/tracks?ids={0}&client_id={1}".format(track_id, client_id)
+logger.info("Requesting track info url: " + track_info_url)
 track_info = requests.get(track_info_url)
-track_name = track_info.json()[0]["title"]
-stream_url = track_info.json()[0]["media"]["transcodings"][0]["url"] # We should change this to search for the hls mp3 stream
-stream_info = requests.get(stream_url + "?client_id={0}".format(client_id))
-playlist_url = stream_info.json()["url"]
 
+track_name = track_info.json()[0]["title"]
+logger.info("Track name: " + track_name)
+
+# We should change this to search for the hls mp3 stream
+stream_url = track_info.json()[0]["media"]["transcodings"][0]["url"] + "?client_id={0}".format(client_id)
+logger.info("Requesting stream url: " + stream_url)
+stream_info = requests.get(stream_url)
+
+playlist_url = stream_info.json()["url"]
+logger.info("Requesting playlist url: " + playlist_url)
 m3u8_obj = m3u8.load(playlist_url)
+
+logger.info("Found {0} files in playlist".format(len(m3u8_obj.files)))
+
 files = list()
-for file_name in m3u8_obj.files:
-    request = requests.get(file_name, stream=True)
+for file_url in m3u8_obj.files:
+    logger.info("Requesting playlist file url: " + file_url)
+    request = requests.get(file_url, stream=True)
     temp = tempfile.NamedTemporaryFile(delete=False)
     received = 0
     with temp as f:
@@ -33,8 +55,13 @@ for file_name in m3u8_obj.files:
         f.seek(0)
         files.append(AudioSegment.from_file_using_temporary_files(f, 'mp3'))
 
-file_name = re.sub(r'(?u)[^-\w]', '', track_name.strip().replace(' ', '_')) + ".mp3" # need to remove special chars to make a valid file name
-
 full_track = functools.reduce(lambda r,next : r+next, files)
-full_track.export(file_name, format="mp3")
-print("Written track \"{0}\" to file {1}".format(track_name, file_name))
+
+# need to remove special chars to make a valid file name
+file_name = re.sub(r'(?u)[^-\w]', '', track_name.strip().replace(' ', '_')) + ".mp3"
+file_path = os.path.join(DATA_PATH, file_name)
+
+logger.info("Exporting track to file: " + file_path)
+full_track.export(file_path, format="mp3")
+
+logger.info("Exported track \"{0}\" to file {1}".format(track_name, file_path))
